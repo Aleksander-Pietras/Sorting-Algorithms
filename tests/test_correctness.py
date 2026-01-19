1f898267db121a434d2aff11af028efa790696d1
Aleksander Pietras <alexpietras0@gmail.com>
2026-01-19 14:07:33 +0000

    created f:test empty list - unfinished

---
+++ tests/test_correctness.py
@@ -0,0 +1,20 @@
+'''
+The functions are used to test the algorithms
+- This file does not generate the lists, lists are generated at data_generation/generators.py
+
+Correctness tests:
+- Empty list
+- One element
+- Already sorted
+- Reverse sorted
+- All elements equal
+- Random integers
+- Negative numbers
+- Duplicates
+- Large list
+'''
+
+def test_empty_list(function: function):
+    pass
+
+test_empty_list()
