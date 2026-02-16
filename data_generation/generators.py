141f223774771945b7077ccd087d571796d9bab2
Aleksander Pietras <alexpietras0@gmail.com>
2026-02-16 20:35:44 +0000

    Add f: generate SORTED array

---
+++ data_generation/generators.py
@@ -0,0 +1,21 @@
+"""
+Docstring for data_generation.generators
+"""
+
+def generate_sorted_array(array_size):
+    """
+    Creates an SORTED array of a size: array_size
+    - unsorted array has all elements 1 -> array_size (inclusive)
+    
+    returns: sorted_array
+    """
+    sorted_array = []
+    for num in range(1, array_size + 1):
+        sorted_array.append(num)
+
+    return sorted_array
+
+
+if __name__ == "__main__":
+    test = generate_sorted_array(100)
+    print(test)
