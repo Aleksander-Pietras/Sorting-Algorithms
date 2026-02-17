582059a2527921faa345c5b4bcaf1a1b87eb758e
Aleksander Pietras <alexpietras0@gmail.com>
2026-02-17 16:39:38 +0000

    Add f: generate usorted array_

--- data_generation/generators.py
+++ data_generation/generators.py
@@ -3,29 +3,42 @@ Docstring for data_generation.generators
 """
 import random
 
-def generate_sorted_array(array_size):
+def generate_sorted_array(array_size: int) -> list[int]:
     """
-    Creates an SORTED array of a size: array_size
+    Creates an SORTED array of a size: array_size + 1
     - sorted array has all elements 1 -> array_size (inclusive)
     
-    :param array_size: Description
+    :param array_size: The max value in the array. Also determines the length of the array.
+    :type array_size: int
     :return: sorted_array
     :rtype: list[int]
     """
     return list(range(1, array_size + 1))
 
-def generate_unsorted_array_efficiently(sorted_array: list[int]):
+def generate_unsorted_array_efficiently(sorted_array: list[int]) -> list[int]:
     """
     Creates an UNSORTED array using a SORTED array. Does NOT create an array in place.
     
-    :param sorted_array: Description
+    :param sorted_array: Array of intigers, in order, length: array_size + 1, with min: 1, and max: array_size. No duplicate values.
     :type sorted_array: list[int]
-
     :return: unsorted_array
     :rtype: list[int]
     """
     random.shuffle(sorted_array) # sorted_array is not shuffled and random
     return sorted_array
+
+def generate_unsorted_array_(array_size: int) -> list[int]:
+    """
+    Creates an USORTED array of size: array_size + 1
+    
+    :param array_size: The max value in the array. Also determines the length of the array.
+    :type array_size: int
+    :return: unsorted_array
+    :rtype: list[int]
+    """
+    array = list(range(1, array_size + 1))
+    random.shuffle(array)
+    return array
 
 if __name__ == "__main__":
     test = generate_sorted_array(100)
