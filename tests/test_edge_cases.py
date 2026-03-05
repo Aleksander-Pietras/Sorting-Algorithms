5f0405e499a28b7af21432b5657d3759196640ea
Aleksander Pietras <alexpietras0@gmail.com>
2026-03-05 01:55:04 +0000

    all edge cases for tests

--- tests/test_edge_cases.py
+++ tests/test_edge_cases.py
@@ -19,5 +19,47 @@ def sort_function(request):
     return request.param
 
 def test_empty_list(sort_function):
-    with pytest.raises(TypeError, match="Input unsorted list cannot be empty"):
-        sort_function([])
+    assert sort_function([]) == []
+
+def test_single_input(sort_function):
+    assert sort_function([1]) == [1]
+
+def test_already_sorted(sort_function):
+    sorted_array = [1, 2, 3, 4, 5, 6]
+    assert sort_function(sorted_array) == sorted_array
+
+    sorted_array_2 = [-3, -1, 0, 1, 5, 10]
+    assert sort_function(sorted_array_2) == sorted_array_2
+
+def test_reverse_sorted(sort_function):
+    sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    reverse_sorted_array = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    assert sort_function(reverse_sorted_array) == sorted_array
+
+@pytest.mark.timeout(2)
+def test_equal_elements(sort_function):
+    array = [1, 1]
+    assert sort_function(array) == array
+
+    array2 = [2, 2, 2, 2, 2, 2]
+    assert sort_function(array2) == array2
+
+def test_random_integers(sort_function):
+    sorted_array = [-124, -77, 7, 15, 20, 88, 234, 301, 6000]
+    unsorted_array = [234, 7, -77, 6000, 20, -124, 15, 301, 88]
+    assert sort_function(unsorted_array) == sorted_array
+    
+def test_random_real_numbers(sort_function):
+    sorted_array = [1.425_111_091, 3.141_592_654, 999.999_999_999]
+    unsorted_array = [999.999_999_999, 1.425_111_091, 3.141_592_654]
+    assert sort_function(unsorted_array) == sorted_array
+
+def test_negative_numbers(sort_function):
+    sorted_array = [-5, -4, -3, -2, -1, 0]
+    unsorted_array = [-1, -4, -5, 0, -2, -3]
+    assert sort_function(unsorted_array) == sorted_array
+
+def test_duplicates(sort_function):
+    sorted_array = [1, 1, 3, 3, 4, 5, 5, 6]
+    unsorted_array = [5, 4, 1, 3, 3, 6, 4, 1]
+    assert sort_function(unsorted_array) == sorted_array
