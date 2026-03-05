46dd89df5677f3ad285a19ffd05bb73502cb7822
Aleksander Pietras <alexpietras0@gmail.com>
2026-03-05 16:45:23 +0000

    added tests for correctness

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -16,3 +16,18 @@ from algorithms.merge_sort import merge_sort
 @pytest.fixture(params=[bubble_sort, merge_sort])
 def sort_function(request):
     return request.param
+
+def test_quantity_stress(sort_function):
+    """
+    Ensure sort function can be ran multiple times without change; in accuracy and time
+    """
+    for _ in range(1000):
+        sorted_array, unsorted_array = [], [] #TODO: get arrays from data generators
+        assert sort_function(unsorted_array) == sorted_array
+
+def test_volume_stress(sort_function):
+    """
+    Ensure stability when working with large datasets
+    """
+    sorted_array, unsorted_array = [], [] #TODO: get arrays from data generators
+    assert sort_function(unsorted_array) == sorted_array
